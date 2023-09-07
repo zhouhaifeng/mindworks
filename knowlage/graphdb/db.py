e5353dbcd22fc9cd4bca84d9cf0da850f042d20b
mills <haifeng.zhou@live.com>
2023-09-07 09:02:07 +0800

    grpahql

---
+++ knowlage/graphdb/db.py
@@ -0,0 +1,61 @@
+from graphql import (
+    GraphQLObjectType,
+    GraphQLSchema,
+    GraphQLString,
+    GraphQLField,
+    parse,
+    execute,
+)
+import numpy as np
+import json
+
+class graphql:
+    def __init__:
+        self.client = None
+        return
+    def search_vector(vectors):
+        # 定义一个类型来表示向量
+        VectorType = GraphQLObjectType(
+            name="Vector",
+            fields=lambda: {
+                "components": GraphQLField(GraphQLString),
+                "magnitude": GraphQLField(GraphQLString),
+            },
+        )
+
+        # 定义一个根查询类型
+        QueryType = GraphQLObjectType(
+            name="Query",
+            fields={
+                "vector": GraphQLField(
+                    VectorType,
+                    args={
+                        "name": GraphQLField(GraphQLString),
+                    },
+                    resolve=lambda root, info, name: {
+                        "components": json.dumps(list(vectors[name])),
+                        "magnitude": str(np.linalg.norm(vectors[name])),
+                    },
+                ),
+            },
+        )
+
+        # 创建GraphQL模式
+        schema = GraphQLSchema(query=QueryType)
+
+        # 处理GraphQL查询
+        query_str = """
+        {
+        vector(name: "vector1") {
+            components
+            magnitude
+        }
+        }
+        """
+
+        query = parse(query_str)
+        result = execute(schema, query)
+
+        # 打印结果
+        print(json.dumps(result.data, indent=2))
+        return result
